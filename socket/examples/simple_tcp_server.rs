34f4e5e272f6dc7a73d75f3d19d8db51eb3ecabb
sprayman1999 <196011564@qq.com>
2023-01-06 00:31:18 +0800

    Update: Initial commit.

--- socket/examples/simple_tcp_server.rs
+++ socket/examples/simple_tcp_server.rs
@@ -1,4 +1,27 @@
-use std ::net::{TcpListener,TcpStream};
+use std::{ 
+    net::{TcpListener},
+    thread, io::{Write, Read}
+};
 fn main(){
-    
+    let tcp_server = TcpListener::bind("127.0.0.1:45678").unwrap();
+    for income in tcp_server.incoming(){
+        thread::spawn(||{
+            let mut client = income.unwrap();
+            println!("{} connect server!",client.peer_addr().unwrap());
+            loop {
+                let mut content_from_client: [u8;4096] = [0;4096];
+                match client.read(&mut content_from_client){
+                    Ok(length) => {
+                        for pos in 0..length{
+                            print!("{}",content_from_client[pos] as char);
+                        }
+                        if length > 0{
+                            client.write("Hello Rust TCP Server!".as_bytes()).unwrap();
+                        }
+                    },
+                    Err(_) => break
+                };
+            }
+        });
+    }
 }
--- socket/examples/simple_udp_server.rs
+++ socket/examples/simple_udp_server.rs
@@ -1,4 +1,21 @@
-use std ::net::{TcpListener,TcpStream};
+use std::{ 
+    net::{UdpSocket},
+};
 fn main(){
-    
+    let udp_socket = UdpSocket::bind("127.0.0.1:45678").unwrap();
+    loop {
+        let mut content_from_client: [u8;4096] = [0;4096];
+        match udp_socket.recv_from(&mut content_from_client){
+            Ok((length,client_addr)) => {
+                println!("{} send msg to server!",client_addr);
+                for pos in 0..length{
+                    print!("{}",content_from_client[pos] as char);
+                }
+                if length > 0{
+                    udp_socket.send_to("Hello Rust UDP Server!".as_bytes(),client_addr).unwrap();
+                }
+            },
+            Err(_) => break
+        };
+    }
 }
