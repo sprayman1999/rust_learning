afa5b751606b98afb7563db6bf977b3be08dc0b0
sprayman1999 <196011564@qq.com>
2023-01-06 23:56:58 +0800

    Update: Initial commit.

---
+++ actix-web-demo/examples/hello_assets.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+
+use actix_files::Files;
+use actix_web::{HttpServer,App,get,Responder,web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result::<()>{
+    HttpServer::new(||{
+        App::new()
+            .service(Files::new("/static","assets/static"))
+            .service(Files::new("/public","assets/public").show_files_listing())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
