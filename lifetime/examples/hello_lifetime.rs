5c47f300bb1a4fb32d4d62c917f23386c30747fd
sprayman1999 <196011564@qq.com>
2023-03-01 01:46:14 +0800

    Update: Initial commit.

--- lifetime/examples/hello_lifetime.rs
+++ lifetime/examples/hello_lifetime.rs
@@ -6,9 +6,20 @@ fn max_num<'a>(x: &'a i32, y: &'a i32) -> &'a i32{
         &y
     }
 }
+fn min_num<'a, 'b>(x: &'a i32, y: &'a i32) -> &'b i32
+    where 'a:'b
+{
+    if x < y {
+        &x
+    }else{
+        &y
+    }
+}
 fn main(){
     let x = 1;
     let y = 2;
     let max = max_num(&x,&y);
     println!("{}",max);
+    let min = min_num(&x,&y);
+    println!("{}",min);
 }
